0a1274992b94ba34bb1fbffb3c1f60c0769cd675
tery <vladislavutkin92@gmail.com>
2019-02-27 17:09:19 +0300

    иcправил условие

--- src/js/app.js
+++ src/js/app.js
@@ -7,15 +7,15 @@ window.onload = function () {
     for (val of costs) {
         sum = val + sum;
         let mod = sum % 10;
-        if (mod ===  1) {
+        if (mod === 1) {
             console.log(mod);
             console.log(`Вы пополнили счет на сумму: ${val}. Ваш баланс: ${sum} балл.\n\n\n `);
         }
-        else if (mod === 0 || 5 || 6 || 7 || 8 || 9) {
+        else if (mod === 0 || mod === 5 || mod === 6 || mod === 7 || mod === 8 || mod === 9) {
             console.log(mod);
             console.log(`Вы пополнили счет на сумму: ${val}. Ваш баланс: ${sum} баллов.\n\n\n `);
         }
-        else if (mod === 2 || 3 || 4) {
+        else if (mod === 2 || mod === 3 || mod === 4) {
             console.log(mod);
             console.log(`Вы пополнили счет на сумму: ${val}. Ваш баланс: ${sum} балла.\n\n\n `);
         }
