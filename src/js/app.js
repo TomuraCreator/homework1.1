6725ac7ee660a175834a368c484a0684785b534e
tery <vladislavutkin92@gmail.com>
2019-02-28 16:00:13 +0300

    изменена структура

--- src/js/app.js
+++ src/js/app.js
@@ -1,23 +1,22 @@
 'use strict';
 
-let costs = [523, 569, 324, 23, 58, 100, 900, 711, 523, 569, 324, 23, 58, 100, 900, 711, 23, 58, 100, 900, 711, 523, 569, 324, 23, ];
-let val;
-let sum = 0;
-window.onload = function () {
-    for (val of costs) {
-        sum = val + sum;
-        let mod = sum % 10;
+const sumOfValue = (value) => {
+    let declination;
+        let mod = value % 10;
         if (mod === 1) {
-            console.log(mod);
-            console.log(`Вы пополнили счет на сумму: ${val}. Ваш баланс: ${sum} балл.\n\n\n `);
+            declination = `балл.`;
         }
         else if (mod === 0 || mod === 5 || mod === 6 || mod === 7 || mod === 8 || mod === 9) {
-            console.log(mod);
-            console.log(`Вы пополнили счет на сумму: ${val}. Ваш баланс: ${sum} баллов.\n\n\n `);
+            declination = `баллов.`;
         }
         else if (mod === 2 || mod === 3 || mod === 4) {
-            console.log(mod);
-            console.log(`Вы пополнили счет на сумму: ${val}. Ваш баланс: ${sum} балла.\n\n\n `);
+            declination = `балла.`;
         }
-    }
+        return `${value}${declination}`;
 };
+
+const value = 567;
+const output = sumOfValue`Ваш баланс: ${value} `;
+console.log(output);
+
+
